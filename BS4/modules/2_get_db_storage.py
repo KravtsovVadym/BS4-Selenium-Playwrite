e649766a35d1981e9797d69d59b2a017fa0f78ee
Vadym <vadymkravtcsov@gmail.com>
2026-02-18 13:05:24 +0200

    feat: add web scraping projects using BS4, Playwright, and Selenium

---
+++ BS4/modules/2_get_db_storage.py
@@ -0,0 +1,23 @@
+"""
+This module connects to Django models using the load_django import,
+reads the json file and writes to the postgres database.
+"""
+
+import json
+from load_django import *
+from parser_app.models import Product
+
+
+def push_db():
+    try:
+        with open("../files/parse_data_brain.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except (FileNotFoundError, ValueError) as e:
+        print(f"File error: {e}")
+        return
+    obj, create = Product.objects.get_or_create(**data)
+    print(f"{obj}, {create}")
+
+
+    
+if __name__ == "__main__": push_db()
